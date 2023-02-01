083f26db09b785a3d425419b8458bf182dcc897c
finlayclark <s1629668@ed.ac.uk>
2023-02-01 11:13:11 +0000

    Create template for run_calc()

--- EnsEquil/run.py
+++ EnsEquil/run.py
@@ -1,4 +1,5 @@
-"""Provide the primary functions."""
+"""Functions for running free energy calculations with SOMD with automated
+ equilibration detection based on an ensemble of simulations."""
 
 
 def canvas(with_attribution=True):
@@ -24,9 +25,23 @@ def canvas(with_attribution=True):
     return quote
 
 
+def test():
+    print("Running calculation...")
+
+
+def run_calc():
+
+    # Create required output directories
+
+    # Run initial SOMD simulations
+
+    # Check the results of the simulations
+
+    # Cycle of analysis/ resubmission as necessary
+
+    # Save data and perform final analysis
+
+
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
     print(canvas())
-
-def run_calc():
-    print("Running calculation...")
