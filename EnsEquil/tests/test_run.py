711acc6105cc0d05afb59bcf2109986a872dd932
finlayclark <s1629668@ed.ac.uk>
2023-02-06 19:32:07 +0000

    Test required directories created

--- EnsEquil/tests/test_run.py
+++ EnsEquil/tests/test_run.py
@@ -4,12 +4,28 @@ Unit and regression test for the EnsEquil package.
 
 # Import package, test suite, and other packages as needed
 import sys
-
-import pytest
+import subprocess
+import os
 
 import EnsEquil
 
+from tempfile import TemporaryDirectory
 
 def test_EnsEquil_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "EnsEquil" in sys.modules
+
+def test_dirs_created():
+    """Check that all expected directories are created"""
+    with TemporaryDirectory() as dirname:
+        subprocess.run(["cp", "-r", "EnsEquil/data/example_input", f"{dirname}/input"])
+        os.chdir(dirname)
+        # This should create output directories
+        EnsEquil.Ensemble()
+
+        lam_dir_names = ["lambda_0.000", "lambda_0.250", "lambda_1.000"]
+        run_names = [f"run_0{i}" for i in range(1, 6)]
+
+        for lam_dir in lam_dir_names:
+            for run in run_names:
+                assert os.path.isdir(os.path.join(dirname, "output", lam_dir, run))
