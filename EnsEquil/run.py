f2c46897880ed9dd790c392fa4777074f9ded953
finlayclark <s1629668@ed.ac.uk>
2023-02-10 13:03:50 +0000

    Create utils module

--- EnsEquil/run.py
+++ EnsEquil/run.py
@@ -9,10 +9,10 @@ import logging as _logging
 from matplotlib import pyplot as _plt
 import numpy as _np
 import pickle as _pkl
-import scipy.stats as _stats
 from time import sleep as _sleep
 from typing import Dict as _Dict, List as _List, Tuple as _Tuple, Any as _Any, Optional as _Optional
 
+from ._utils import plot as _plot
 
 class Ensemble():
     """
@@ -479,7 +479,7 @@ class LamWindow():
             equilibrated = True
 
         # Save plots of dh/dl and d_dh/dl
-        plot(x_vals = times,
+        _plot(x_vals = times,
              y_vals= _np.array([self._get_rolling_average(dh_dl, idx_block_size) for dh_dl in dh_dls]),
              x_label= "Simulation Time per Window per Run / ns",
              y_label= r"$\frac{\mathrm{d}h}{\mathrm{d}\lambda}$ / kcal mol$^{-1}$", 
@@ -488,7 +488,7 @@ class LamWindow():
             # delay in the block average calculation.
              vline_val= equil_time + 1 * self.block_size if equil_time else None)
 
-        plot(x_vals = times,
+        _plot(x_vals = times,
              y_vals= _np.array(d_dh_dls),
              x_label= "Simulation Time per Window per Run / ns",
              y_label= r"$\frac{\partial}{\partial t}\frac{\partial H}{\partial \lambda}$ / kcal mol$^{-1}$ ns$^{-1}$", 
@@ -786,50 +786,3 @@ class Simulation():
         for var in vars(self):
             self._logger.debug(f"{var}: {getattr(self, var)} ")
         self._logger.debug("##############################################")
-
-
-def plot(x_vals: _np.ndarray, y_vals: _np.ndarray, x_label: str, y_label: str, 
-         outfile: str, vline_val: _Optional[float] = None, 
-         hline_val: _Optional[float] = None) -> None:
-    """ 
-    Plot several sets of y_vals against one set of x vals, and show confidence
-    intervals based on inter-y-set deviations (assuming normality).
-
-    Parameters
-    ----------
-    x_vals : np.ndarray
-        1D array of x values.
-    y_vals : np.ndarray
-        1 or 2D array of y values, with shape (n_sets, n_vals). Assumes that
-        the sets of data are passed in the same order as the runs.
-    x_label : str
-        Label for the x axis.
-    y_label : str
-        Label for the y axis.
-    outfile : str
-        Name of the output file.
-    vline_val : float, Optional
-        x value to draw a vertical line at, for example the time taken for
-        equilibration.
-    hline_val : float, Optional
-        y value to draw a horizontal line at.
-    """
-    y_avg = _np.mean(y_vals, axis=0)
-    conf_int = _stats.t.interval(0.95, len(y_vals[:,0])-1, loc=y_avg, scale=_stats.sem(y_vals,axis=0)) # 95 % C.I.
-
-    fig, ax = _plt.subplots(figsize=(8,6))
-    ax.plot(x_vals, y_avg, label="Mean", linewidth=2)
-    for i, entry in enumerate(y_vals):
-        ax.plot(x_vals, entry, alpha=0.5, label=f"run {i+1}")
-    if vline_val is not None:
-        ax.axvline(x=vline_val, color='red', linestyle='dashed')
-    if hline_val is not None:
-        ax.axhline(y=hline_val, color='black', linestyle='dashed')
-    # Add confidence intervals
-    ax.fill_between(x_vals, conf_int[0], conf_int[1], alpha=0.5, facecolor='#ffa500')
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
-    ax.legend()
-
-    fig.savefig(outfile, dpi=300, bbox_inches='tight')
-    
