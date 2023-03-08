a18c14a474581987bd1ecf65a6096ed77d839288
finlayclark <s1629668@ed.ac.uk>
2023-03-08 19:36:31 +0000

    Fix tests

--- EnsEquil/tests/test_stage.py
+++ EnsEquil/tests/test_stage.py
@@ -3,9 +3,10 @@ Unit and regression test for the run module.
 """
 
 # Import package, test suite, and other packages as needed
+import logging
+import os
 import sys
 import subprocess
-import os
 
 import EnsEquil
 
@@ -21,16 +22,17 @@ def test_dirs_created():
         # Store current working directory to change back to later
         cwd = os.getcwd()
         subprocess.run(["cp", "-r", "EnsEquil/data/example_input", f"{dirname}/input"])
-        os.chdir(dirname)
         # This should create output directories
-        EnsEquil.Stage(stage_type=EnsEquil.StageType.DISCHARGE)
+        EnsEquil.Stage(stage_type=EnsEquil.StageType.DISCHARGE, 
+                       input_dir=f"{dirname}/input",
+                       base_dir=dirname,
+                       output_dir=f"{dirname}/output",
+                       stream_log_level=logging.WARNING)
 
         lam_dir_names = ["lambda_0.000", "lambda_0.250", "lambda_1.000"]
         run_names = [f"run_0{i}" for i in range(1, 6)]
 
         for lam_dir in lam_dir_names:
             for run in run_names:
-                assert os.path.isdir(os.path.join(dirname, "output", lam_dir, run))
+                assert os.path.isdir(os.path.join(dirname, lam_dir, run))
 
-        # Change back to original working directory
-        os.chdir(cwd)
