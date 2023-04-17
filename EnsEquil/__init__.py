5062cbe3bb614e28261f3e1a846d7a355cfd9233
Finlay Clark <finlayclark@ed.ac.uk>
2023-04-17 19:39:02 +0000

    Update __init__

--- EnsEquil/__init__.py
+++ EnsEquil/__init__.py
@@ -1,8 +1,9 @@
 """A package for running free energy calculations with SOMD with automated equilibration detection based on an ensemble of simulations."""
 
 # Run imports
-from .run import calculation, leg, stage, lambda_window, simulation
+from .run import calculation, enums, leg, stage, lambda_window, simulation
 from .run.calculation import *
+from .run.enums import *
 from .run.leg import *
 from .run.stage import *
 from .run.lambda_window import *
