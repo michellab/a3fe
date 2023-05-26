6b2d3db3537e8fea5cd6f87ef1e7cb3a466f7503
Finlay Clark <finlayclark@ed.ac.uk>
2023-05-26 10:30:03 +0000

    Update example input to run on plato

--- EnsEquil/data/example_run_dir/input/run_somd.sh
+++ EnsEquil/data/example_run_dir/input/run_somd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -o somd-array-gpu-%A.%a.out
-#SBATCH -p main
+#SBATCH -p RTX3080,GTX980
 #SBATCH -n 1
 #SBATCH --time 24:00:00
 #SBATCH --gres=gpu:1
@@ -8,5 +8,5 @@
 lam=$1
 echo "lambda is: " $lam
 
-srun /home/finlayclark/sire.app/bin/somd-freenrg -C somd.cfg -l $lam -p CUDA
+srun somd-freenrg -C somd.cfg -l $lam -p CUDA
 
