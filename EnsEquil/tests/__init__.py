e451991cef3f461660654817f6ecaa26f3cb941f
Finlay Clark <finlayclark@ed.ac.uk>
2023-07-06 14:27:33 +0000

    Check GROMACS_PRESENT in innit

--- EnsEquil/tests/__init__.py
+++ EnsEquil/tests/__init__.py
@@ -4,6 +4,9 @@ import shutil
 # Check if slurm is present
 SLURM_PRESENT = False if shutil.which("sbatch") is None else True
 
+# Check if gromacs is present
+GROMACS_PRESENT = False if shutil.which("gmx") is None else True
+
 # See if the user wants to skip the slurm tests
 RUN_SLURM_TESTS = True
 if os.environ.get("RUN_SLURM_TESTS") == "False":
