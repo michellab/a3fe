240073fc9ab612c192451e658c755982ef19b3c2
Finlay Clark <finlayclark@ed.ac.uk>
2023-07-06 13:11:15 +0000

    Check that tests are being run from root directory

--- EnsEquil/tests/__init__.py
+++ EnsEquil/tests/__init__.py
@@ -8,3 +8,9 @@ SLURM_PRESENT = False if shutil.which("sbatch") is None else True
 RUN_SLURM_TESTS = True
 if os.environ.get("RUN_SLURM_TESTS") == "False":
     RUN_SLURM_TESTS = False
+
+# Make sure that we're in the correct directory and raise an error if not
+if not os.path.exists("EnsEquil"):
+    raise RuntimeError(
+        "Please run the tests from the root directory of the repository."
+    )
