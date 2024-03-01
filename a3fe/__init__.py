874f84b2bc74c6276a447620238caab23a8435dd
finlayclark <s1629668@ed.ac.uk>
2024-03-01 12:56:39 +0000

    Automatically increase soft limit on number of open files

--- a3fe/__init__.py
+++ a3fe/__init__.py
@@ -3,8 +3,23 @@
 # This package was previously named "EnsEquil". To allow objects pickled with the
 # old name to be unpickled with the new name, we set EnsEquil to point to a3fe.
 import sys as _sys
+import warnings as _warnings
 
 _sys.modules["EnsEquil"] = _sys.modules["a3fe"]
+
+# A3FE can open many files due to the use of multiprocessing and
+# threading with MBAR. This can cause a "Too many open files" error.
+# The following code increases the number of open files allowed to the
+# hard limit.
+import resource as _resource
+
+_nofile_soft, _nofile_hard = _resource.getrlimit(_resource.RLIMIT_NOFILE)
+if _nofile_soft < _nofile_hard:
+    _warnings.warn(
+        f"Soft limit for number of open files ({_nofile_soft}) is less than the hard limit ({_nofile_hard})."
+        " Increasing the soft limit to the hard limit."
+    )
+    _resource.setrlimit(_resource.RLIMIT_NOFILE, (_nofile_hard, _nofile_hard))
 
 
 # Run imports
