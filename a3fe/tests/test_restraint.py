20556d4b05813755568ab51d0205ac30521d9bde
finlayclark <s1629668@ed.ac.uk>
2024-03-28 16:03:11 +0000

    Update restraint pickle so it can be loaded by new sire

--- a3fe/tests/test_restraint.py
+++ a3fe/tests/test_restraint.py
@@ -10,12 +10,12 @@ def test_create_a3fe_restraint(bss_restraint):
     a3fe_restraint = A3feRestraint(bss_restraint)
     assert isinstance(a3fe_restraint, A3feRestraint)
     assert (
-        a3fe_restraint.somd_restr_string
-        == 'boresch restraints dictionary = {"anchor_points":{"r1":1330, "r2":1318, "r3":1332, "l1":28, "l2":6, "l3":21}, "equilibrium_values":{"r0":7.05, "thetaA0":1.32, "thetaB0":1.43,"phiA0":-2.78, "phiB0":1.21, "phiC0":0.91}, "force_constants":{"kr":3.91, "kthetaA":31.06, "kthetaB":70.95, "kphiA":26.92, "kphiB":66.33, "kphiC":99.93}}'
+        a3fe_restraint.toString()
+        == 'boresch restraints dictionary = {"anchor_points":{"r1":803, "r2":801, "r3":818, "l1":28, "l2":6, "l3":21}, "equilibrium_values":{"r0":9.67, "thetaA0":1.46, "thetaB0":1.33,"phiA0":-1.81, "phiB0":2.25, "phiC0":2.44}, "force_constants":{"kr":8.11, "kthetaA":54.75, "kthetaB":115.30, "kphiA":159.43, "kphiB":167.55, "kphiC":83.78}}'
     )
     assert a3fe_restraint.getCorrection(method="analytical").value() == approx(
-        -9.854938427174691, abs=1e-6
+        -10.753345568838798, abs=1e-6
     )
     assert a3fe_restraint.getCorrection(method="numerical").value() == approx(
-        -9.858136706200451, abs=1e-6
+        -10.755515978356776, abs=1e-6
     )
