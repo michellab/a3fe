99be80c8f6709ac4f566667f1ca50f3c5562b808
finlayclark <s1629668@ed.ac.uk>
2025-02-03 16:54:51 +0000

    Move system prep config attribute descriptions to the fields

--- a3fe/configuration/system_prep_config.py
+++ a3fe/configuration/system_prep_config.py
@@ -24,61 +24,33 @@ from typing import List as _List, Dict as _Dict
 class _BaseSystemPreparationConfig(_ABC, _BaseModel):
     """
     Pydantic model for holding system preparation configuration.
-
-    Attributes
-    ----------
-    slurm: bool
-        Whether to use SLURM for the preparation.
-    forcefields : dict
-        Forcefields to use for the ligand, protein, and water.
-    water_model : str
-        Water model to use.
-    ion_conc : float
-        Ion concentration in M.
-    steps : int
-        Number of steps for the minimisation.
-    runtime_short_nvt : int
-        Runtime for the short NVT equilibration in ps.
-    runtime_nvt : int
-        Runtime for the NVT equilibration in ps.
-    end_temp : float
-        End temperature for the NVT equilibration in K.
-    runtime_npt : int
-        Runtime for the NPT equilibration in ps.
-    runtime_npt_unrestrained : int
-        Runtime for the unrestrained NPT equilibration in ps.
-    ensemble_equilibration_time : int
-        Ensemble equilibration time in ps.
-    append_to_ligand_selection: str
-        If this is a bound leg, this appends the supplied string to the default atom
-        selection which chooses the atoms in the ligand to consider as potential anchor
-        points. The default atom selection is f'resname {ligand_resname} and not name H*'.
-        Uses the mdanalysis atom selection language. For example, 'not name O*' will result
-        in an atom selection of f'resname {ligand_resname} and not name H* and not name O*'.
-    lambda_values: dict[legtype, dict[stagetype, list[float]]]
-        the lambda values to use for each stage of each leg.
     """
 
-    slurm: bool = _Field(True)
-    forcefields: dict = {
-        "ligand": "openff_unconstrained-2.0.0",
-        "protein": "ff14SB",
-        "water": "tip3p",
-    }
-    water_model: str = "tip3p"
-    ion_conc: float = _Field(0.15, ge=0, lt=1)  # M
-    steps: int = _Field(1000, gt=0, lt=100_000)  # This is the default for _BSS
-    runtime_short_nvt: int = _Field(5, gt=0, lt=500)  # ps
-    runtime_nvt: int = _Field(50, gt=0, lt=5_000)  # ps
-    end_temp: float = _Field(298.15, gt=0, lt=350)  # K
-    runtime_npt: int = _Field(400, gt=0, lt=40_000)  # ps
-    runtime_npt_unrestrained: int = _Field(1000, gt=0, lt=100_000)  # ps
-    ensemble_equilibration_time: int = _Field(5000, gt=0, lt=50_000)  # ps
+    slurm: bool = _Field(True, description="Whether to use SLURM for the preparation.")
+    forcefields: dict = _Field(
+        default={
+            "ligand": "openff_unconstrained-2.0.0",
+            "protein": "ff14SB",
+            "water": "tip3p",
+        },
+        description="Forcefields to use for the ligand, protein, and water.",
+    )
+    water_model: str = _Field("tip3p", description="Water model to use.")
+    ion_conc: float = _Field(0.15, ge=0, lt=1, description="Ion concentration in M.")
+    steps: int = _Field(1000, gt=0, lt=100_000, description="Number of steps for the minimisation.")
+    runtime_short_nvt: int = _Field(5, gt=0, lt=500, description="Runtime for the short NVT equilibration in ps.")
+    runtime_nvt: int = _Field(50, gt=0, lt=5_000, description="Runtime for the NVT equilibration in ps.")
+    end_temp: float = _Field(298.15, gt=0, lt=350, description="End temperature for the NVT equilibration in K.")
+    runtime_npt: int = _Field(400, gt=0, lt=40_000, description="Runtime for the NPT equilibration in ps.")
+    runtime_npt_unrestrained: int = _Field(1000, gt=0, lt=100_000, description="Runtime for the unrestrained NPT equilibration in ps.")
+    ensemble_equilibration_time: int = _Field(5000, gt=0, lt=50_000, description="Ensemble equilibration time in ps.")
     append_to_ligand_selection: str = _Field(
         "",
-        description="Atom selection to append to the ligand selection during restraint searching.",
+        description="If this is a bound leg, this appends the supplied string to the default atom selection which chooses the atoms in the ligand to consider as potential anchor points. The default atom selection is f'resname {ligand_resname} and not name H*'. Uses the mdanalysis atom selection language. For example, 'not name O*' will result in an atom selection of f'resname {ligand_resname} and not name H* and not name O*'.",
     )
-    lambda_values: _Dict[_LegType, _Dict[_StageType, _List[float]]]
+    lambda_values: _Dict[_LegType, _Dict[_StageType, _List[float]]] = _Field(
+        ..., description="The lambda values to use for each stage of each leg."
+    )
 
     model_config = _ConfigDict(extra="forbid", validate_assignment=True)
 
@@ -167,67 +139,64 @@ class _BaseSystemPreparationConfig(_ABC, _BaseModel):
 class SomdSystemPreparationConfig(_BaseSystemPreparationConfig):
     """
     Pydantic model for holding system preparation configuration
-    for running simulations with SLURM.
-
-    Attributes
-    ----------
-    lambda_values: dict[legtype, dict[stagetype, list[float]]]
-        the lambda values to use for each stage of each leg.
+    for running simulations with SOMD.
     """
-
-    lambda_values: _Dict[_LegType, _Dict[_StageType, _List[float]]] = {
-        _LegType.BOUND: {
-            _StageType.RESTRAIN: [0.0, 1.0],
-            _StageType.DISCHARGE: [0.0, 0.291, 0.54, 0.776, 1.0],
-            _StageType.VANISH: [
-                0.0,
-                0.026,
-                0.054,
-                0.083,
-                0.111,
-                0.14,
-                0.173,
-                0.208,
-                0.247,
-                0.286,
-                0.329,
-                0.373,
-                0.417,
-                0.467,
-                0.514,
-                0.564,
-                0.623,
-                0.696,
-                0.833,
-                1.0,
-            ],
+    lambda_values: _Dict[_LegType, _Dict[_StageType, _List[float]]] = _Field(
+        default={
+            _LegType.BOUND: {
+                _StageType.RESTRAIN: [0.0, 1.0],
+                _StageType.DISCHARGE: [0.0, 0.291, 0.54, 0.776, 1.0],
+                _StageType.VANISH: [
+                    0.0,
+                    0.026,
+                    0.054,
+                    0.083,
+                    0.111,
+                    0.14,
+                    0.173,
+                    0.208,
+                    0.247,
+                    0.286,
+                    0.329,
+                    0.373,
+                    0.417,
+                    0.467,
+                    0.514,
+                    0.564,
+                    0.623,
+                    0.696,
+                    0.833,
+                    1.0,
+                ],
+            },
+            _LegType.FREE: {
+                _StageType.DISCHARGE: [0.0, 0.222, 0.447, 0.713, 1.0],
+                _StageType.VANISH: [
+                    0.0,
+                    0.026,
+                    0.055,
+                    0.09,
+                    0.126,
+                    0.164,
+                    0.202,
+                    0.239,
+                    0.276,
+                    0.314,
+                    0.354,
+                    0.396,
+                    0.437,
+                    0.478,
+                    0.518,
+                    0.559,
+                    0.606,
+                    0.668,
+                    0.762,
+                    1.0,
+                ],
+            },
         },
-        _LegType.FREE: {
-            _StageType.DISCHARGE: [0.0, 0.222, 0.447, 0.713, 1.0],
-            _StageType.VANISH: [
-                0.0,
-                0.026,
-                0.055,
-                0.09,
-                0.126,
-                0.164,
-                0.202,
-                0.239,
-                0.276,
-                0.314,
-                0.354,
-                0.396,
-                0.437,
-                0.478,
-                0.518,
-                0.559,
-                0.606,
-                0.668,
-                0.762,
-                1.0,
-            ],
-        },
-    }
+        description="The lambda values to use for each stage of each leg.",
+    )
 
 
 ENGINE_TYPE_TO_SYSPREP_CONFIG = {
