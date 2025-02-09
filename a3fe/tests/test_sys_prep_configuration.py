d4f4253ffb178185ceaabf84b37ea240f39184df
Roy-Haolin-Du <s2015679@ed.ac.uk>
2025-02-09 22:25:50 +0000

    change for importng new SomdSystemPreparationConfig

--- a3fe/tests/test_sys_prep_configuration.py
+++ a3fe/tests/test_sys_prep_configuration.py
@@ -4,24 +4,24 @@ from tempfile import TemporaryDirectory
 
 import pytest
 
-from a3fe import LegType, SystemPreparationConfig
+from a3fe import LegType, SomdSystemPreparationConfig
 
 
 def test_create_default_config():
     """Test that the default config is created correctly."""
-    config = SystemPreparationConfig()
+    config = SomdSystemPreparationConfig()
     assert config.ensemble_equilibration_time == 5000
 
 
 def test_incorrect_config():
     """Test that an incorrect config raises an error."""
     with pytest.raises(ValueError):
-        SystemPreparationConfig(ensemble_equilibration_time=-1)
+        SomdSystemPreparationConfig(ensemble_equilibration_time=-1)
 
 
 def test_incorrect_config_mod():
     """Test that modifying the default config incorrectly raises an error."""
-    config = SystemPreparationConfig()
+    config = SomdSystemPreparationConfig()
     with pytest.raises(ValueError):
         config.ensemble_equilibration_time = -1
 
@@ -29,14 +29,14 @@ def test_incorrect_config_mod():
 def test_config_dump_and_load():
     """Test that the config can be pickled and loaded."""
     with TemporaryDirectory() as dirname:
-        config = SystemPreparationConfig()
+        config = SomdSystemPreparationConfig()
         config.dump(dirname, LegType.FREE)
-        config2 = SystemPreparationConfig.load(dirname, LegType.FREE)
+        config2 = SomdSystemPreparationConfig.load(dirname, LegType.FREE)
         assert config == config2
 
 
 def test_config_tot_simtime():
     """Test that the total simulation time is calculated correctly."""
-    config = SystemPreparationConfig()
+    config = SomdSystemPreparationConfig()
     assert config.get_tot_simtime(n_runs=5, leg_type=LegType.FREE) == 26855
     assert config.get_tot_simtime(n_runs=5, leg_type=LegType.BOUND) == 26905
